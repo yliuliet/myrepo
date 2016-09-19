c8b062149eb7dbd1e09b995b6600a5c23988028f
yliuliet <yliuliet8410@gmail.com>
2016-09-19 12:26:06 +0900

    second commit

--- initiatedata.py
+++ initiatedata.py
@@ -1,11 +1,19 @@
-# データを生成してプロットする
-from numpy.random import *
-from sklearn import *
-from sklearn import datasets
-
-import matplotlib.pyplot as plt
+#!/usr/bin/env python
+# coding:utf-8
 import numpy as np
-import sklearn as skl
-np.random.seed(0)
-X, y = skl.datasets.make_moons(200, noise=0.20)
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+import chainer.functions as F
+from chainer import Variable, FunctionSet, optimizers
+
+#モデルを作る
+model = F.Linear(3,3)
+
+#これがモデルに与えるベクトル
+data = np.array([[1,2,3]] , dtype=np.float32)
+
+x = Variable(np.array(data))
+
+#ここでモデルにベクトルを投げて、加工させている
+y = model(x)
+
+#モデルが加工したベクトルを表示
+print(y.data) 
